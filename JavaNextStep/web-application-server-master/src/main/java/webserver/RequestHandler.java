3262638173fd6f54c55f3d6243a0ab2aba8179c1
park <qmzo553@gmail.com>
2024-04-15 09:03:08 +0900

    2024.04.15 3.4.3.3

--- JavaNextStep/web-application-server-master/src/main/java/webserver/RequestHandler.java
+++ JavaNextStep/web-application-server-master/src/main/java/webserver/RequestHandler.java
@@ -3,7 +3,6 @@ package webserver;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -32,51 +31,65 @@ public class RequestHandler extends Thread {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream();
-        BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
+        try (InputStream in = connection.getInputStream();
+                OutputStream out = connection.getOutputStream();
+                BufferedReader br = new BufferedReader(new InputStreamReader(in))) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             String line = br.readLine();
-            while(!"".equals(line)) {
-                if(line == null) {
-                    return;
-                }
-                System.out.println(line);
-                
-                String[] tokens = line.split(" ");
-                String method = tokens[0];
-                String request = tokens[1];
-                String version = tokens[2];
+            if (line == null) {
+                return;
+            }
+            System.out.println(line);
 
-                if(method.equals("GET")) {
-                    String path = request;
-                    String data;
+            String[] tokens = line.split(" ");
+            String method = tokens[0];
+            String request = tokens[1];
+            String version = tokens[2];
 
-                    if(request.contains("?")) {
-                        int index = request.indexOf('?');
-                        path = request.substring(0, index);
-                        data = request.substring(index + 1);
+            if (method.equals("GET")) {
+                String path = request;
+                String data;
 
-                        Map<String, String> parameters = HttpRequestUtils.parseQueryString(data);
-                        User user = new User(
+                if (request.contains("?")) {
+                    int index = request.indexOf('?');
+                    path = request.substring(0, index);
+                    data = request.substring(index + 1);
+
+                    Map<String, String> parameters = HttpRequestUtils.parseQueryString(data);
+                    User user = new User(
                             parameters.get("userId"),
                             parameters.get("password"),
                             parameters.get("name"),
                             parameters.get("email"));
-                    }
-                    
-                    byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
-                    
-                    DataOutputStream dos = new DataOutputStream(out);
-                    response200Header(dos, MAX_PRIORITY);
-                    responseBody(dos, body);
                 }
-                line = br.readLine();
-            }
 
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+                byte[] body = Files.readAllBytes(new File("./webapp" + path).toPath());
+
+                DataOutputStream dos = new DataOutputStream(out);
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            } else if (method.equals("POST")) {
+                String path = request;
+                Map<String, String> fields = new HashMap<>();
+
+                line = br.readLine();
+                while (!"".equals(line)) {
+                    String[] requestTokens = line.split(" ");
+                    String field = requestTokens[0].substring(0, requestTokens[0].length() - 1);
+                    String fileldValue = requestTokens[1];
+
+                    fields.put(field, fileldValue);
+                    line = br.readLine();
+                }
+
+                line = br.readLine();
+                Map<String, String> parameters = HttpRequestUtils.parseQueryString(line);
+                    User user = new User(
+                            parameters.get("userId"),
+                            parameters.get("password"),
+                            parameters.get("name"),
+                            parameters.get("email"));
+            }
         } catch (IOException e) {
             log.error(e.getMessage());
         }
