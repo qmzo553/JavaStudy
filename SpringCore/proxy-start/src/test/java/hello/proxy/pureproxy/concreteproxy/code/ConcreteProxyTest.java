0603158eaf4d86a2b9fef4ef293c1b5ee7d10f03
park <qmzo553@gmail.com>
2024-06-11 07:48:22 +0900

    2024-06-10

---
+++ SpringCore/proxy-start/src/test/java/hello/proxy/pureproxy/concreteproxy/code/ConcreteProxyTest.java
@@ -0,0 +1,23 @@
+package hello.proxy.pureproxy.concreteproxy.code;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+class ConcreteProxyTest {
+
+    @Test
+    void noProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        ConcreteClient client = new ConcreteClient(concreteLogic);
+        client.execute();
+    }
+
+    @Test
+    void addProxy() {
+        ConcreteLogic concreteLogic = new ConcreteLogic();
+        TimeProxy timeProxy = new TimeProxy(concreteLogic);
+        ConcreteClient client = new ConcreteClient(timeProxy);
+        client.execute();
+    }
+}
