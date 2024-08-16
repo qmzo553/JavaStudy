af2a29f27be5753eca19a2bae848f0d24f735d92
qmzo553 <qmzo553@gmail.com>
2024-08-16 18:33:03 +0900

    20240816

---
+++ Algorithm/Recursive/Q15657.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q15657 {
+
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] ans;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        ans = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        dfs(0,0);
+
+        System.out.println(sb);
+    }
+
+    public static void dfs(int start, int depth) {
+        if(depth == m) {
+            for(int num : ans) {
+                sb.append(num).append(' ');
+            }
+
+            sb.append('\n');
+            return;
+        }
+
+        for(int i = start; i < n; i++) {
+            ans[depth] = arr[i];
+            dfs( i,depth + 1);
+        }
+    }
+}
