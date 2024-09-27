da5b6e8bcc6ce5fcd08500cd3a4f24243a358a0a
qmzo553 <qmzo553@gmail.com>
2024-09-27 23:20:49 +0900

    20240927

---
+++ Algorithm/DynamicProgramming/Q11051.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q11051 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][n + 1];
+
+        for(int i = 0; i < dp.length; i++) {
+            for(int j = 0; j <= i; j++) {
+                if(i == j || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+                }
+            }
+        }
+
+        System.out.println(dp[n][k]);
+    }
+}
